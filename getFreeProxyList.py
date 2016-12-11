53b63776afa67c39a7f9383234000841ac82aedb
Srefan <liyangkobebryant@gmail.com>
2016-12-11 10:28:21 +0800

    解决macOS开机python找不到module的问题

--- getFreeProxyList.py
+++ getFreeProxyList.py
@@ -2,31 +2,35 @@
 
 import sys
 import urllib2
-import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 def getIpList():
 	url = 'http://api.xicidaili.com/free2016.txt'
-	data = requests.get(url)
-	data = data.text
-	ip_list = data.split('\r\n')
-	return ip_list
+	try:
+		data = urllib2.urlopen(url, timeout=10).read()
+		ip_list = data.split('\r\n')
+		return ip_list
+	except:
+		return []
 	
-def getKuaiIpList():
-	resultArray = []
-	url = 'http://www.kuaidaili.com/free/outha'
-	response = urllib2.urlopen(url).read()
+# def getKuaiIpList():
+# 	resultArray = []
+# 	url = 'http://www.kuaidaili.com/free/outha'
+# 	try:
+# 		response = urllib2.urlopen(url, timeout=10).read()
 
-	soup = BeautifulSoup(response, "html.parser")
-	tbody = soup.find('tbody')
-	for tag in tbody.find_all('tr'):
-		ip = tag.find(attrs={"data-title": "IP"}).text
-		port = tag.find(attrs={"data-title": "PORT"}).text
-		resultArray.append(ip + ':' + port)
-	return resultArray
+# 		soup = BeautifulSoup(response, "html.parser")
+# 		tbody = soup.find('tbody')
+# 		for tag in tbody.find_all('tr'):
+# 			ip = tag.find(attrs={"data-title": "IP"}).text
+# 			port = tag.find(attrs={"data-title": "PORT"}).text
+# 			resultArray.append(ip + ':' + port)
+# 	except:
+# 		pass
+# 	return resultArray
 
  
 if __name__=='__main__':
 	reload(sys)
 	sys.setdefaultencoding("utf-8")
-	print getKuaiIpList()
+	print getIpList()
