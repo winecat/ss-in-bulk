4bed2e1a5c345027f8ef4b9eff52b608e2d56d59
liyang <liyang@everyoo.com>
2016-12-12 14:15:59 +0800

    添加手动快速重启的脚本

--- getFreeSs.py
+++ getFreeSs.py
@@ -5,19 +5,31 @@ import urllib2
 import json
 
 def getFreeSsInfos():
-	try:
-		url = "https://api.mianvpn.com/ajax.php?verify=true&mod=getfreess"
-		response = urllib2.urlopen(url, timeout=10)
-		hjson = json.loads(response.read())
-		data = ''
-		for i in xrange(0, len(hjson)):
-			data += hjson[i]['i'] + ' ' + hjson[i]['p'] + ' ' + hjson[i]['m'] + ' ' + hjson[i]['pw'] + ' ' + hjson[i]['r']
-			data += '\n'
-		return data
-	except:
-		return ''
+    url = "http://api.jiasu.im/api/apiv2.php?op=tourist"
+
+    try:
+        # url = "https://api.mianvpn.com/ajax.php?verify=true&mod=getfreess"
+        response = urllib2.urlopen(url, timeout=10)
+        result = json.loads(response.read())
+
+        hjson = result['msg']['ss']
+        data = ''
+        for i in xrange(0, len(hjson)):
+            data += hjson[i]['serverip']
+            data += ' ' 
+            data += hjson[i]['port']
+            data += ' '
+            data += hjson[i]['authscheme']
+            data += ' '
+            data += hjson[i]['passwd']
+            data += ' '
+            data += hjson[i]['servername']
+            data += '\n'
+        return data
+    except:
+        return ''
 
 if __name__=='__main__':
-	reload(sys)
-	sys.setdefaultencoding("utf-8")
-	print(getFreeSsInfos())
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    print(getFreeSsInfos())
---
+++ manual.sh
@@ -0,0 +1,46 @@
+## ShadowsocksX 批量配置 by @cjli
+# 如果没有 ss.xml 请在确保具有相应权限下先执行如下命令:
+# cp ~/Library/Preferences/clowwindy.ShadowsocksX.plist ~/
+# plutil -convert xml1 clowwindy.ShadowsocksX.plist -o ss.xml
+
+date +%T-%Y/%m/%d
+
+# 切换路径
+filepath=$(cd "$(dirname "$0")"; pwd)
+cd $filepath
+
+# 0. 从MianVPN获取免费的SS账号 写入到配置文件
+python getFreeSs.py > ss.cnf
+
+# 0. 判断获取到的SS账号,如果未获取到中止运行
+if [ "`md5 ss.cnf`" == 'MD5 (ss.cnf) = 68b329da9893e34099c7d8ad5cb9c940' ]; then
+    exit 1
+fi
+
+# 0. 计算MD5 如果一致就不做更新 不一致保存新的MD5
+#if [ "`md5 ss.cnf`" == "`cat md5.txt`" ]; then  
+#	exit 1  
+#fi
+#md5 ss.cnf > md5.txt
+
+# 1. 读取配置文件 servers.conf 生成新的 Base64 配置字符串并替换掉 ss.xml 中的 <data> 标签
+php -f ss.php > result.php
+
+# 1. 判断配置文件转换是否出错
+if [ -s result.php ]; then
+	exit 1
+fi
+
+# 2. 将新的 ss.xml 转换为 plist
+plutil -convert binary1 ss.xml -o clowwindy.ShadowsocksX.plist
+
+# 3. 用新的 plist 替换掉原来的 plist
+defaults import clowwindy.ShadowsocksX clowwindy.ShadowsocksX.plist
+
+# 4. 然后重新打开 ShadowsockX ( End )
+killall ShadowsocksX 1>/dev/null 2>/dev/null
+sleep 1s
+open /Applications/ShadowsocksX.app/
+
+# 返回路径
+cd - >/dev/null
